0241c3932a0c57e5d3081bce790667a13fce2e03
GUCLU Sefa <sefaaguclu@gmail.com>
2021-11-26 16:55:16 +0100

    test

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ let app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('views'))
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 app.listen(3000, () => console.log('Le serveur est actif !'))
 app.use('/', Routeur = require('./routes/routes'))
--- controllers/ctrlAccueil.js
+++ controllers/ctrlAccueil.js
@@ -1,6 +1,6 @@
 var modelAccueil = require('../models/modelAccueil');
 module.exports = {
     afficher_accueil: function (req, res) {
-        res.render('./accueil')
+        res.render('./header')
     },
 }
