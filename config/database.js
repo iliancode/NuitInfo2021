c8d8f2462fb0aa3f4825c8293832af44c6b00c5d
GUCLU Sefa <sefaaguclu@gmail.com>
2021-11-26 14:16:14 +0100

    languages

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', 
-    password: '',  
-    database: 'nuitinfo' 
+    host: 'mysql-nuitinfo.alwaysdata.net',
+    user: 'nuitinfo', 
+    password: 'nuitinfo2021lab',  
+    database: 'nuitinfo_2021' 
 });
 conn.connect(function (err) {
     if (err) throw err;
