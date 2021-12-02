bfe1e616850480e67dae14f47929308fa6a61535
Sefa GUCLU <sefaaguclu@gmail.com>
2021-12-02 22:13:54 +0100

    listes

--- controllers/ctrlAccueil.js
+++ controllers/ctrlAccueil.js
@@ -1,8 +1,11 @@
 var modelAccueil = require('../models/modelAccueil');
 module.exports = {
+    //afficher accueil
     afficher_accueil: function (req, res) {
         res.render('./accueil')
     },
+
+    // afficher formulaires
     afficher_ajouterBateau: function (req, res) {
         res.render('./ajouterBateau')
     },
@@ -11,5 +14,18 @@ module.exports = {
     },
     afficher_ajouterSauvetage: function (req, res) {
         res.render('./ajouterSauvetage')
+    },
+
+    //afficher listes
+    afficher_listeBateaux: function (req, res) {
+        res.render('./listeBateaux')
+    },
+
+    afficher_listeSauveuteurs: function (req, res) {
+        res.render('./listeSauveuteur')
+    },
+
+    afficher_listeSauvetages: function (req, res) {
+        res.render('./listeSauvetages')
     },
 }
--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,14 @@ const routeur = express.Router();
 var ctrlAccueil = require('../controllers/ctrlAccueil')
 
 routeur.get('/', ctrlAccueil.afficher_accueil)
+//formulaires
 routeur.get('/ajouter_bateau', ctrlAccueil.afficher_ajouterBateau)
 routeur.get('/ajouter_sauveuteur', ctrlAccueil.aficher_ajouterSauveuteur)
-*routeur.get('/ajouter_sauvetage', ctrlAccueil.afficher_ajouterSauvetage)
+routeur.get('/ajouter_sauvetage', ctrlAccueil.afficher_ajouterSauvetage)
+// listes
+routeur.get('/liste_bateaux', ctrlAccueil.afficher_listeBateaux)
+routeur.get('/liste_sauveuteurs', ctrlAccueil.afficher_listeSauveuteurs)
+routeur.get('/liste_sauvetages', ctrlAccueil.afficher_listeSauvetages)
 
 
 module.exports = routeur;
