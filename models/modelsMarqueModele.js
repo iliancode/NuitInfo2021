db8b8d383fd9b433d78056aea924263d6d20b594
YanisHlali <yanis.hlali@gmail.com>
2021-12-03 01:35:31 +0100

    TEST

--- models/modelsMarqueModele.js
+++ models/modelsMarqueModele.js
@@ -7,7 +7,7 @@ async function creerMarque(marque) {
     db.query(`INSERT INTO marques (Marque_nom) VALUES ('${marque}')`,(err,result) => {
         if (err) throw err;
     })
-}
+}t
 
 async function creerModele(modele) {
     db.query(`INSERT INTO modeles (Modele_Nom) VALUES ('${modele})`,(err,result) => {
