8cb5cb534dc462140aad17ba6454fdf8bcf68756
Sefa GUCLU <sefaaguclu@gmail.com>
2021-12-03 06:33:55 +0100

    comit

--- controllers/ctrlSauveteur.js
+++ controllers/ctrlSauveteur.js
@@ -1,28 +1,22 @@
-var modelsSauveteur = require('../models/modelsSauveteur');
+var modelsSauveteur = require("../models/modelsSauveteur");
 module.exports = {
-    afficher_ajouterSauveteur: function (req, res) {
-        res.render('./ajouterSauveteur')
-    },
-    afficher_modifierSauveteur: function (req, res) {
-        res.render('./modifierSauveteur')
-    },
-    afficher_listeSauveteurs: function (req, res) {
-        res.render('./listeSauveteurs')
-    },
-    executer_sauveteur: function (req, res) {
-        let nom = req.body.inputNom;
-        let prenom = req.body.inputPrenom;
+  afficher_ajouterSauveteur: function (req, res) {
+    res.render("./ajouterSauveteur");
+  },
+  afficher_modifierSauveteur: function (req, res) {
+    res.render("./modifierSauveteur");
+  },
+  executer_sauveteur: function (req, res) {
+    let nom = req.body.inputNom;
+    let prenom = req.body.inputPrenom;
 
-    
-        modelSauveuteur.creerSauveteur(nom,prenom, function (data) {
-    
-        });
-        res.redirect("./liste_sauveteurs");
-      },
-    
-    
-    afficher_listeSauveteurs:async function (req,res) {
-        let donnees = await modelsSauveteur.recupererSauveteur();
-        res.render('./liste_sauveteurs', {donnees : donnees})
-    }
-}
+    modelSauveuteur.creerSauveteur(nom, prenom, function (data) {});
+    res.redirect("./liste_sauveteurs");
+  },
+
+  afficher_listeSauveteurs: async function (req, res) {
+    modelsSauveteur.lireSauveteur(function (data) {
+        res.render('./liste_sauveteurs', { donnees: data});
+    });
+  },
+};
