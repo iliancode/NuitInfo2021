22907643874ca1ac7e7c058ebd36637d126a472e
YanisHlali <yanis.hlali@gmail.com>
2021-12-03 02:56:27 +0100

    vfgbhj

--- models/modelsBateaux.js
+++ models/modelsBateaux.js
@@ -1,9 +1,9 @@
 var db = require("../config/database");
 
 async function creerBateau(nom, idMarque, idModele, type, constructeur) {
-  let requete = `INSERT INTO bateaux (Bateau_Nom,Bateau_idMarque,Bateau_idModele,Bateau_Type)`;
-  requete += ` VALUES ('${nom}',${idMarque},${idModele},'${type}')`;
-  db.query(requete, (err, result) => {
+  let req = `INSERT INTO bateaux (Bateau_Nom,Bateau_idMarque,Bateau_idModele,Bateau_Type)`;
+  req += ` VALUES ('${nom}',${idMarque},${idModele},'${type}')`;
+  db.query(req, (err, result) => {
     if (err) throw err;
   });
 }
@@ -18,11 +18,11 @@ async function lireBateau(id) {
 }
 
 async function MettreAJourBateau(nom,modele,marque,id) {
-    let requete = `UPDATE bateaux SET Bateau_Nom=${nom},`
-    requete += `Bateau_Modele=${modele},`
-    requete += `Bateau_Marque=${marque} `
-    requete += `WHERE Bateau_id=${id}`
-    db.query(requete,(err,result) => {
+    let req = `UPDATE bateaux SET Bateau_Nom=${nom},`
+    req += `Bateau_Modele=${modele},`
+    req += `Bateau_Marque=${marque} `
+    req += `WHERE Bateau_id=${id}`
+    db.query(req,(err,result) => {
         if (err) throw err;
     })
 }
